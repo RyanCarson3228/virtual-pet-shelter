b76ce60cef596b538c71079a2f6fc0a56e3c459d
RyanCarson3228 <rcarson95@gmail.com>
2018-02-06 07:40:07 -0500

    Building App

--- src/main/java/virtualpetshelter/VirtualPetShelter.java
+++ src/main/java/virtualpetshelter/VirtualPetShelter.java
@@ -8,23 +8,32 @@ public class VirtualPetShelter {
 
 	private Map<String, VirtualPet> pets = new HashMap<String, VirtualPet>();
 
-	public void addPet(VirtualPet virtualPet) {
-		pets.put(virtualPet.getName(), virtualPet);
+	// return a Collection of all of the pets in the shelter
+	public Collection<VirtualPet> pets() {
+		return pets.values();
 	}
 
-	public void removePet(VirtualPet virtualPet) {
-		pets.remove(virtualPet.getName());
+	// return a specific VirtualPet given its name
+	public VirtualPet getPet(String name) {
+		return pets.get(name);
+	}
 
+	// allow intake of a homeless pet
+	public void addPet(VirtualPet virtualPet) {
+		pets.put(virtualPet.getName(), virtualPet);
 	}
 
 	public int listSize() {
 		return pets.size();
 	}
 
-	public VirtualPet getPet(String name) {
-		return pets.get(name);
+	// allow adoption of a homeless pet
+	public void removePet(VirtualPet virtualPet) {
+		pets.remove(virtualPet.getName());
+
 	}
 
+	// Extract attributes from VirtualPet
 	public int getPetThirst(String name) {
 		return pets.get(name).getThirst();
 	}
@@ -37,6 +46,68 @@ public class VirtualPetShelter {
 		return pets.get(name).getBoredom();
 	}
 
+	// water all pets in the shelter
+	public void petDrink(String name) {
+		VirtualPet virtualPet = pets.get(name);
+		virtualPet.drink();
+	}
+
+	public void drinkAll() {
+		for (VirtualPet virtualPet : pets()) {
+			petDrink(virtualPet.getName());
+		}
+	}
+
+	// feed all pets in the shelter
+	public void feedPet(String name) {
+		VirtualPet virtualPet = pets.get(name);
+		virtualPet.feed();
+	}
+
+	public void feedAll() {
+		for (VirtualPet virtualPet : pets()) {
+			feedPet(virtualPet.getName());
+		}
+	}
+
+	// plays with an individual pet in the shelter
+	public void petPlay(String name) {
+		VirtualPet virtualPet = pets.get(name);
+		virtualPet.play();
+	}
+
+	// include a tick method that calls the tick method for each of the pets in the
+	// shelter, updating their needs
+	public void tickPet(String name) {
+		VirtualPet virtualPet = pets.get(name);
+		virtualPet.tick();
+	}
+
+	public void allTick() {
+		for (VirtualPet virtualPet : pets()) {
+			tickPet(virtualPet.getName());
+		}
+	}
+
+	//console methods
+	public void printShelterStatus() {
+		System.out.println("This is the Status of the the pets at the Shelter:");
+		System.out.println("Name\t\t|Thirst\t|Hunger\t|Boredom");
+		System.out.println("--------|-------|------|-------");
+		for (VirtualPet virtualPet : pets()) {
+			System.out.println(virtualPet);
+			System.out.print(virtualPet.getName() + "\t\t|" + virtualPet.getThirst() + "\t|");
+			System.out.println(virtualPet.getHunger() + "\t|" + virtualPet.getBoredom());
+		}
+
+	}
+
+	public void NameAndDescription() {
+		for (VirtualPet virtualPet : pets()) {
+			System.out.println("[" + virtualPet.getName() + "] " + virtualPet.getDescription() + ".");
+		}
+	}
+	
 	
 
 }
--- src/main/java/virtualpetshelter/VirtualPetShelterApp.java
+++ src/main/java/virtualpetshelter/VirtualPetShelterApp.java
@@ -1,10 +1,27 @@
 package virtualpetshelter;
 
+import java.util.Scanner;
+
 public class VirtualPetShelterApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		
+		VirtualPetShelter shelter = new VirtualPetShelter();
+		
+		System.out.println("Welcome to your Pet Shelter!");
+		System.out.println("It's our job to take care of stray pets until we can find them new homes!");
+		System.out.println("Lets start by adding our first pet.");
+		System.out.println("Since it's a stray, we'll have to give it a new name:");
+		String name = input.nextLine();
+		System.out.println("And let's give it a brief description for potential adopters:");
+		String description = input.nextLine();
+		shelter.addPet(new VirtualPet(name, description));
+		
+		System.out.println("If at any time you would like to quit, just type \"quit.\"");
+		
+		
 	}
 
 }
